3256b3165bd87bdbba20a45c5387e0104eaf49ea
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-08-07 16:48:47 +0700

    initiate

---
+++ src/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+  <google-map name="example">
+  </google-map>
+</template>
+
+<script>
+import GoogleMap from './GoogleMap'
+
+export default {
+  components: { GoogleMap },
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
