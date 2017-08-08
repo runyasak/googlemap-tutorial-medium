383f2c97e1a1c93cfafac2307011705cb3e6b345
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-08-08 17:56:03 +0700

    add 2 markers on map

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -1,53 +1,71 @@
 <template>
-  <div class="google-map" :id="mapName"></div>
+  <div>
+    <div class="google-map" :id="mapName"></div>
+    <button @click="justClick">Click Me</button>
+  </div>
 </template>
+
 <script>
+/* global google */
+
 export default {
   name: 'google-map',
   props: ['name'],
-  data: function () {
+  data () {
     return {
-      mapName: this.name + "-map",
+      mapName: this.name + '-map',
       markerCoordinates: [{
-        latitude: 51.501527,
-        longitude: -0.1921837
+        latitude: 13.801959,
+        longitude: 100.575090
       }, {
-        latitude: 51.505874,
-        longitude: -0.1838486
-      }, {
-        latitude: 51.4998973,
-        longitude: -0.202432
+        latitude: 13.770238,
+        longitude: 100.572120
       }],
       map: null,
       bounds: null,
       markers: []
     }
   },
-  mounted: function () {
-    const inter = setInterval(() => {
-      if (google) {
-        this.createMap()
-        clearInterval(inter)
-      }
-    }, 1000)
+  mounted () {
+    // navigator.geolocation.getCurrentPosition(function (location) {
+    //   console.log(location.coords.latitude)
+    //   console.log(location.coords.longitude)
+    //   console.log(location.coords.accuracy)
+    // }, function (err) {
+    //   console.log(err)
+    // }, {timeout: 10000})
+    this.createMap()
   },
   methods: {
     createMap () {
-      this.bounds = new google.maps.LatLngBounds()
-      const element = document.getElementById(this.mapName)
-      const mapCentre = this.markerCoordinates[0]
-      const options = {
-        center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude)
-      }
-      this.map = new google.maps.Map(element, options)
-      this.markerCoordinates.forEach((coord) => {
-        const position = new google.maps.LatLng(coord.latitude, coord.longitude)
-        const marker = new google.maps.Marker({ 
-          position,
-          map: this.map
-        })
-      this.markers.push(marker)
-        this.map.fitBounds(this.bounds.extend(position))
+      const operate = setInterval(() => {
+        if (google) {
+          this.bounds = new google.maps.LatLngBounds()
+          const element = document.getElementById(this.mapName)
+          // const mapCentre = this.markerCoordinates[0]
+          const options = {
+            zoom: 14
+            // center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude)
+          }
+          this.map = new google.maps.Map(element, options)
+          console.log(this.map)
+          this.markerCoordinates.forEach((coord) => {
+            const markerPos = new google.maps.LatLng(coord.latitude, coord.longitude)
+            const marker = new google.maps.Marker({
+              position: markerPos,
+              map: this.map
+            })
+            this.markers.push(marker)
+            this.map.fitBounds(this.bounds.extend(markerPos))
+          })
+          clearInterval(operate)
+        }
+      }, 1000)
+    },
+    justClick () {
+      console.log(this.getLocation())
+      this.getLocation().then((res) => {
+        console.log(res)
       })
     }
   }
