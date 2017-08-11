3d4002844352fa4666c5072bd2bcf579bada4b4c
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2017-08-11 14:49:13 +0700

    refactor code

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -37,6 +37,7 @@ export default {
       mapName: this.name + '-map',
       currentPosition: {
         name: 'FlyingComma (here)',
+        icon: 'https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.0-9/20664138_10209348452935128_8696622600378619713_n.jpg?oh=b42f621ead91ee2f5991e672141dd066&oe=5A381B32',
         latitude: 13.801959,
         longitude: 100.575090
       },
@@ -63,30 +64,35 @@ export default {
       return this.markerCoordinates
     }
   },
-  mounted () {
-    this.checkGoogle().then(elem => {
-      this.initiateMap()
-      this.generateDistance()
-    })
+  async mounted () {
+    await this.checkGoogle()
+    this.initiateMap()
+    this.generateDistance()
+    // this.getCurrentLocation().then(pos => {
+    //   console.log(pos)
+    // })
   },
   methods: {
     checkGoogle () {
       return new Promise((resolve, reject) => {
-        let operate = setInterval(() => {
-          if (google) {
-            resolve(google)
-            clearInterval(operate)
-          }
-        }, 100)
+        try {
+          let operate = setInterval(() => {
+            if (google) {
+              resolve(google)
+              clearInterval(operate)
+            }
+          }, 100)
+        } catch (err) {
+          console.log(err)
+        }
       })
     },
     initiateMap () {
       // Bound Map
       this.bounds = new google.maps.LatLngBounds()
       let element = document.getElementById(this.mapName)
-      let mapCenter = this.currentPosition
-      this.createMap(element, mapCenter)
-      this.addMapMarkder(this.markerCoordinates)
+      this.createMap(element, this.currentPosition)
+      this.generateMapMarker(this.markerCoordinates)
     },
     createMap (element, mapCenter) {
       const options = {
@@ -94,17 +100,28 @@ export default {
         center: new google.maps.LatLng(mapCenter.latitude, mapCenter.longitude)
       }
       this.map = new google.maps.Map(element, options)
+      this.generateMapMarker(mapCenter)
     },
-    addMapMarkder (markers, bound) {
-      markers.forEach((coord) => {
-        const markerPos = new google.maps.LatLng(coord.latitude, coord.longitude)
-        const marker = new google.maps.Marker({
-          position: markerPos,
-          map: this.map
+    generateMapMarker (markers, bound) {
+      if (Array.isArray(markers)) {
+        markers.forEach((coord) => {
+          let markerPos = new google.maps.LatLng(coord.latitude, coord.longitude)
+          this.addMarker(markerPos, null, bound)
         })
-        this.markers.push(marker)
-        if (bound) this.map.fitBounds(this.bounds.extend(markerPos))
+      } else {
+        let markerPos = new google.maps.LatLng(markers.latitude, markers.longitude)
+        let image = 'https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.0-9/20664138_10209348452935128_8696622600378619713_n.jpg?oh=b42f621ead91ee2f5991e672141dd066&oe=5A381B32'
+        this.addMarker(markerPos, image, bound)
+      }
+    },
+    addMarker (pos, image, bound) {
+      let marker = new google.maps.Marker({
+        position: pos,
+        icon: image || '',
+        map: this.map
       })
+      this.markers.push(marker)
+      if (bound) this.map.fitBounds(this.bounds.extend(pos))
     },
     generateDistance () {
       this.markerCoordinates.forEach((marker) => {
@@ -114,6 +131,20 @@ export default {
     },
     rad (x) {
       return x * Math.PI / 180
+    },
+    getCurrentLocation () {
+      return new Promise((resolve, reject) => {
+        try {
+          navigator.geolocation.getCurrentPosition(function (location) {
+            console.log(location.coords.latitude)
+            console.log(location.coords.longitude)
+            console.log(location.coords.accuracy)
+            resolve(location.coords)
+          })
+        } catch (error) {
+          reject(new Error(error))
+        }
+      })
     },
     calculateDistance (p1, p2) {
       let googleP1 = new google.maps.LatLng(p1.latitude, p1.longitude)
@@ -149,8 +180,9 @@ span.partner-distance  {
   font-size: 0.85rem; 
   color: #95A5A6;
 }
+/* ICON */
 .fa-icon {
   width: auto;
-  height: 0.85rem; /* or any other relative font sizes */
+  height: 0.85rem;
 }
 </style>
